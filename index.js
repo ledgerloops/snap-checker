74fdd1466616284aa0309c49808d9c653dc5a873
Michiel de Jong <michiel@unhosted.org>
2016-11-13 08:09:26 +0700

    Remove old drft code

---
+++ index.js
@@ -0,0 +1,16 @@
+var Peer = require('ledgerloops-peer');
+var Routing = require('ddcd-dfs');
+
+function Agent() {
+  this._routing = new Routing();
+  this._peers = {};
+};
+
+Agent.prototype.addPeer = function(nick, sendToExternal) {
+  this._peers[nick] = new Peer(nick, sendToExternal,
+      this._routing.send, this._routing.updateNeighborStatus);
+};
+
+Agent.prototype.getPeer = function(nick) {
+  return this._peers[nick];
+};
