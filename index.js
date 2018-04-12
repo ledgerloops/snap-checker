425d0699c5bd4f910470140d41975860e72174c1
Michiel de Jong <michiel@ripple.com>
2018-04-12 06:50:13 -0700

    sweeps

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const NUM_NODES=5
-const NUM_EDGES=5
+const NUM_NODES=100
+const NUM_EDGES=150
+const EPSILON=1
 
 function randomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -21,6 +22,7 @@ for (let i = 0; i < NUM_EDGES; i++) {
   edges[from].push({
     min: -1 - randomInt(100),
     max,
+    from,
     to,
     current: randomInt(max)
   })
@@ -55,7 +57,7 @@ function isSame (loop1, loop2) {
 }
 
 function reportLoop(loop) {
-  console.log('reportLoop', loop)
+  // console.log('reportLoop', loop)
   for (let i = 0; i < loops.length; i++) {
     if (isSame(loops[i], loop)) {
       return
@@ -65,7 +67,7 @@ function reportLoop(loop) {
 }
 
 function walk(current, path /*, global: loops */) {
-  console.log('walk', path, current)
+  // console.log('walk', path, current)
   for (let search = 0; search < path.length; search++) {
     if (path[search] === current) {
       reportLoop(normalizeLoop(path.slice(search)))
@@ -88,4 +90,67 @@ for (let i = 0; i < NUM_NODES; i++) {
   let loops = []
   walk(i, path, loops)
 }
-console.log(loops)
+console.log('loops', loops.length)
+
+function getEdge(from, to) {
+  for (let i = 0; i < edges[from].length; i++) {
+    if (edges[from][i].to === to) {
+      return edges[from][i]
+    }
+  }
+}
+
+function changeCurrent(from, to, change) {
+  for (let i = 0; i < edges[from].length; i++) {
+    if (edges[from][i].to === to) {
+      edges[from][i].current += change
+      console.log('changing!', from, i, edges[from][i], change)
+      return
+    }
+  }
+  console.log('oops not found!')
+}
+
+function getLoopEdges(loop) {
+  let edges = []
+  for (let i = 1; i < loop.length; i++) {
+    edges.push(getEdge(loop[i-1], loop[i]))
+  }
+  edges.push(getEdge(loop[loop.length - 1], loop[0]))
+  return edges
+}
+
+function sweep() {
+  let totalChange = 0
+  for (let i = 0; i < loops.length; i++) {
+    const edges = getLoopEdges(loops[i]).sort((a, b) => (a.current - b.current))
+    // console.log('got edges', loops[i], edges)
+    let balanceToChange = - edges[Math.floor(edges.length / 2)].current
+    // console.log({ balanceToChange })
+    for (let j = 0; j < edges.length; j++) {
+      if (edges[j].current + balanceToChange <= edges[j].min) {
+        // console.log('too low', balanceToChange, edges[j])
+        balanceToChange = edges[j].min - edges[j].current + EPSILON
+      }
+      if (edges[j].current + balanceToChange >= edges[j].max) {
+        // console.log('too high', balanceToChange, edges[j])
+        balanceToChange = edges[j].max - edges[j].current - EPSILON
+      }
+    }
+    if (balanceToChange === 0) {
+      // console.log('alas! no wiggle room')
+      continue
+    }
+    for (let j = 0; j < edges.length; j++) {
+      changeCurrent(edges[j].from, edges[j].to, balanceToChange)
+      totalChange += balanceToChange
+    }
+  }
+  return totalChange
+}
+
+let changed = 0
+do {
+  changed = sweep()
+  console.log('sweep!', changed)
+} while(changed)
