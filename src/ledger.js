b56991458e102bcc112c29cbd2ce1dfd6c81e06f
Michiel de Jong <michiel@unhosted.org>
2018-09-14 11:36:05 +0200

    COND/FULFILL handling to peerHandler

--- src/ledger.js
+++ src/ledger.js
@@ -13,7 +13,7 @@ function verifyHex(preimageHex, hashHex) {
   return Buffer.from(hashHex, 'hex').equals(correctHash);
 }
 
-function Ledger(peerNick, myNick, unit, agent, medium) {
+function Ledger(peerNick, myNick, unit, handler, medium) {
   this._peerNick = peerNick;
   this._myNick = myNick;
   this._unit = unit;
@@ -27,9 +27,8 @@ function Ledger(peerNick, myNick, unit, agent, medium) {
   };
   this._committed = {};
   this._pendingMsg = {};
-  this._pendingCond = {};
   this._probesReceived = { cwise: [], fwise: [] };
-  this._agent = agent;
+  this._handler = handler;
   this.myNextId = 0;
   console.log({ medium });
   if (typeof medium === 'object' && medium.addChannel) {
@@ -93,14 +92,14 @@ Ledger.prototype = {
     // This Ledger said it's usable, so we should start a loop
     // But let's just double-check the balances, and choose a loop amount of half the diff:
 
-    const fsideBal = this._agent._peerHandlers[fsidePeer].getBalance(); // our fside balance should be low because it will go up
+    const fsideBal = this._handler._agent._peerHandlers[fsidePeer].getBalance(); // our fside balance should be low because it will go up
     const csideBal = this.getBalance();  // our cside balance should be high because it will go down
     const diff = csideBal - fsideBal;
     const amount = diff/2;
     debug.log('using loop', this._myNick, { fsideBal, csideBal, diff, amount });
     const preimage = randomBytes(256);
     const hashHex = sha256(preimage).toString('hex');
-    this._agent._preimages[hashHex] = preimage;
+    this._handler._agent._preimages[hashHex] = preimage;
     if (amount <0) {
       debug.log('amount below zero!', amount);
       panic();
@@ -121,10 +120,10 @@ Ledger.prototype = {
     console.log('storing as if it were an fside probe from', this._peerNick, newProbe);
     this._probesReceived.fwise.push(newProbe); // pretend it came from them, to detect loops later
     const thisBal = this.getBalance();
-    for(let fsideLedger in this._agent._peerHandlers) {
+    for(let fsideLedger in this._handler._agent._peerHandlers) {
       // Our fside balance will go down, so find one whose balance is higher:
-      if (this._agent._peerHandlers[fsideLedger].getBalance() > thisBal) {
-        this._agent._peerHandlers[fsideLedger].send({
+      if (this._handler._agent._peerHandlers[fsideLedger].getBalance() > thisBal) {
+        this._handler._agent._peerHandlers[fsideLedger].send({
           msgType: 'PROBES',
           cwise: [],
           fwise: [ newProbe ]
@@ -200,8 +199,8 @@ Ledger.prototype = {
     this._probesReceived.cwise = this._probesReceived.cwise.concat(msg.cwise);
     this._probesReceived.fwise = this._probesReceived.fwise.concat(msg.fwise);
     const thisBal = this.getBalance();
-    for(let otherPeer in this._agent._peerHandlers) {
-      this._agent._peerHandlers[otherPeer].considerProbe(thisBal, msg, this._peerNick);
+    for(let otherPeer in this._handler._agent._peerHandlers) {
+      this._handler._agent._peerHandlers[otherPeer].considerProbe(thisBal, msg, this._peerNick);
     }
   },
 
@@ -250,7 +249,7 @@ Ledger.prototype = {
         this._pendingMsg[`${msg.sender}-${msg.msgId}`] = msg;
         debug.log('COND - COND - COND', this._myNick, this._pendingMsg);
         if (!outgoing) {
-          setTimeout(() => this._handleCond(msg), 100);
+          setTimeout(() => this._handler._handleCond(msg), 100);
         }
         break;
       }
@@ -276,7 +275,7 @@ Ledger.prototype = {
         delete this._pendingMsg[`${msg.sender}-${msg.msgId}`];
         debug.log('Committed', msg);
         if (!outgoing) {
-          this._handleFulfill(msg);
+          this._handler._handleFulfill(msg);
         }
         break;
       }
@@ -298,85 +297,6 @@ Ledger.prototype = {
   },
   getBalance: function() {
     return this._currentBalance[this._myNick] - this._currentBalance[this._peerNick];
-  },
-
-  _handleCond: function(msg) {
-    debug.log(`Agent ${this._myNick} handles COND that comes in from ${this._peerNick}`, msg);
-    if (this._agent._preimages[msg.condition]) {
-      debug.log('replying with fulfill!', msg.condition, this._agent._preimages[msg.condition].toString('hex'))
-      const reply = {
-        msgType: 'FULFILL',
-        msgId: msg.msgId,
-        sender: this._peerNick,
-        preimage: this._agent._preimages[msg.condition].toString('hex')
-      };
-      this.send(reply);
-    } else {
-      debug.log('hashlock not mine', this._myNick, msg.condition, Object.keys(this._agent._preimages));
-      let suggestLowerAmount = false;
-      const thisBal = this.getBalance();
-      for(let toNick in this._agent._peerHandlers) {
-        debug.log('considering a forward to', toNick, thisBal, this._agent._peerHandlers[toNick].getBalance());
-        // when fowarding a COND, your incoming balance will increase and your outgoing balance will decrease
-        // so it's useful if your outgoing balance is currently higher:
-        const relBal = this._agent._peerHandlers[toNick].getBalance() - thisBal;
-        // example: outBal is 4, inBal is 1; relBal is 3, amount is 2;
-        // afterwards, outBal will be 2 and inBal will be 3, so relBal will be -1 (which is closer to zero than current 3)
-        if (relBal > msg.amount) { // neighbor is higher, forward it
-          debug.log('forwarding!', relBal, msg.amount, this._peerNick, this._myNick, toNick);
-          forwardMsg = this._agent._peerHandlers[toNick].create(msg.amount, msg.condition, msg.routeId);
-          this._agent._peerHandlers[toNick]._ledger._pendingCond[msg.msgId] = {
-            fromNick: this._peerNick,
-            toNick,
-            msg
-          };
-          debug.log(`${this._myNick} is forwarding COND from ${this._peerNick} to ${toNick}`, msg);
-          debug.log(`Probes seen at incoming peer`, this._probesReceived);
-          debug.log(`Probes seen at outgoing peer`, this._agent._peerHandlers[toNick]._probesReceived);
-
-          this._agent._peerHandlers[toNick].send(forwardMsg);
-          return;
-        } else if (relBal > 0) {
-          suggestLowerAmount = true;
-        } else {
-          debug.log(`I don't want to forward this COND from ${this._peerNick} to ${toNick} because my balance with ${toNick} is ${this._agent._peerHandlers[toNick].getBalance()} and my balance with ${this._peerNick} is ${thisBal}`);
-        }
-      }
-      this.send({
-        msgType: 'REJECT',
-        sender: msg.sender,
-        msgId: msg.msgId,
-        reason: (suggestLowerAmount ? 'try a lower amount' : 'not my hashlock and no onward route found')
-      });
-    }
-  },
-
-  _handleFulfill: function(msg) {
-    // TODO: check whether the preimage is valid
-    if (this._pendingCond[msg.msgId]) {
-      const backer = this._pendingCond[msg.msgId].fromNick;
-      debug.log('handling fulfill, backer found:', backer);
-      // FIXME: sending this ACK after the FULFILL has already committed the transaction confuses things!
-      // this.send({
-      //   msgType: 'ACK',
-      //   sender: this._myNick,
-      //   msgId: msg.msgId
-      // });
-      debug.log('cond-level orig:', this._pendingCond[msg.msgId]);
-      const backMsg = {
-        msgType: 'FULFILL',
-        sender: backer,
-        msgId: this._pendingCond[msg.msgId].msg.msgId,
-        preimage: msg.preimage
-      };
-      debug.log(`Passing on FULFILL ${this._peerNick} -> ${this._myNick} -> ${backer}`, backMsg);
-      this._agent._peerHandlers[backer].send(backMsg);
-    } else {
-      debug.log(this._myNick + ': cannot find backer, I must have been the loop initiator.');
-    }
-  },
-
-  _handleReject: function(msg) {
   }
 };
 
--- src/peerhandler.js
+++ src/peerhandler.js
@@ -1,7 +1,12 @@
+const debug = require('./debug');
 var Ledger = require('./ledger');
 
 function PeerHandler(peerNick, myNick, unit, agent, medium) {
-  this._ledger = new Ledger(peerNick, myNick, unit, agent, medium);
+  this._peerNick = peerNick;
+  this._myNick = myNick;
+  this._agent = agent;
+  this._ledger = new Ledger(peerNick, myNick, unit, this, medium);
+  this._pendingCond = {};
 }
 
 PeerHandler.prototype = {
@@ -17,6 +22,85 @@ PeerHandler.prototype = {
   },
   create: function(amount, hashHex, routeId) {
     return this._ledger.create(amount, hashHex, routeId);
+  },
+
+  _handleCond: function(msg) {
+    debug.log(`Agent ${this._myNick} handles COND that comes in from ${this._peerNick}`, msg);
+    if (this._agent._preimages[msg.condition]) {
+      debug.log('replying with fulfill!', msg.condition, this._agent._preimages[msg.condition].toString('hex'))
+      const reply = {
+        msgType: 'FULFILL',
+        msgId: msg.msgId,
+        sender: this._peerNick,
+        preimage: this._agent._preimages[msg.condition].toString('hex')
+      };
+      this.send(reply);
+    } else {
+      debug.log('hashlock not mine', this._myNick, msg.condition, Object.keys(this._agent._preimages));
+      let suggestLowerAmount = false;
+      const thisBal = this.getBalance();
+      for(let toNick in this._agent._peerHandlers) {
+        debug.log('considering a forward to', toNick, thisBal, this._agent._peerHandlers[toNick].getBalance());
+        // when fowarding a COND, your incoming balance will increase and your outgoing balance will decrease
+        // so it's useful if your outgoing balance is currently higher:
+        const relBal = this._agent._peerHandlers[toNick].getBalance() - thisBal;
+        // example: outBal is 4, inBal is 1; relBal is 3, amount is 2;
+        // afterwards, outBal will be 2 and inBal will be 3, so relBal will be -1 (which is closer to zero than current 3)
+        if (relBal > msg.amount) { // neighbor is higher, forward it
+          debug.log('forwarding!', relBal, msg.amount, this._peerNick, this._myNick, toNick);
+          forwardMsg = this._agent._peerHandlers[toNick].create(msg.amount, msg.condition, msg.routeId);
+          this._agent._peerHandlers[toNick]._pendingCond[msg.msgId] = {
+            fromNick: this._peerNick,
+            toNick,
+            msg
+          };
+          debug.log(`${this._myNick} is forwarding COND from ${this._peerNick} to ${toNick}`, msg);
+          debug.log(`Probes seen at incoming peer`, this._ledger._probesReceived);
+          debug.log(`Probes seen at outgoing peer`, this._agent._peerHandlers[toNick]._ledger._probesReceived);
+          //FIXME: https://github.com/ledgerloops/ledgerloops/issues/32
+          this._agent._peerHandlers[toNick].send(forwardMsg);
+          return;
+        } else if (relBal > 0) {
+          suggestLowerAmount = true;
+        } else {
+          debug.log(`I don't want to forward this COND from ${this._peerNick} to ${toNick} because my balance with ${toNick} is ${this._agent._peerHandlers[toNick].getBalance()} and my balance with ${this._peerNick} is ${thisBal}`);
+        }
+      }
+      this.send({
+        msgType: 'REJECT',
+        sender: msg.sender,
+        msgId: msg.msgId,
+        reason: (suggestLowerAmount ? 'try a lower amount' : 'not my hashlock and no onward route found')
+      });
+    }
+  },
+
+  _handleFulfill: function(msg) {
+    // TODO: check whether the preimage is valid
+    if (this._pendingCond[msg.msgId]) {
+      const backer = this._pendingCond[msg.msgId].fromNick;
+      debug.log('handling fulfill, backer found:', backer);
+      // FIXME: sending this ACK after the FULFILL has already committed the transaction confuses things!
+      // this.send({
+      //   msgType: 'ACK',
+      //   sender: this._myNick,
+      //   msgId: msg.msgId
+      // });
+      debug.log('cond-level orig:', this._pendingCond[msg.msgId]);
+      const backMsg = {
+        msgType: 'FULFILL',
+        sender: backer,
+        msgId: this._pendingCond[msg.msgId].msg.msgId,
+        preimage: msg.preimage
+      };
+      debug.log(`Passing on FULFILL ${this._peerNick} -> ${this._myNick} -> ${backer}`, backMsg);
+      this._agent._peerHandlers[backer].send(backMsg);
+    } else {
+      debug.log(this._myNick + ': cannot find backer, I must have been the loop initiator.');
+    }
+  },
+
+  _handleReject: function(msg) {
   }
 };
 
