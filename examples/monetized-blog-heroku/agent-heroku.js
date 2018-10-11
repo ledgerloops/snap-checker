b711871cb85176e8cfb37055f7757fff88992819
Michiel de Jong <michiel@unhosted.org>
2018-10-11 12:45:09 +0200

    hello postgres

--- examples/monetized-blog-heroku/agent-heroku.js
+++ examples/monetized-blog-heroku/agent-heroku.js
@@ -1,6 +1,11 @@
-var Agent = require('../..').Agent;
+const Agent = require('../..').Agent;
+const agent = new Agent('blogger', 'payme');
 
-var agent = new Agent('blogger', 'payme');
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
 
 const fs = require('fs');
 const htmlPage = fs.readFileSync('./examples/monetized-blog-heroku/index.html');
@@ -24,5 +29,21 @@ if (typeof process.env.TESTNET_FRIENDS == 'string') {
   });
 }
 
+async function runSql(query) {
+  try {
+    const client = await pool.connect();
+    const result = await client.query(query);
+    const results = { 'results': (result) ? result.rows : null};
+    return results;
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+}
+
+runSql('SELECT now();').then(result => {
+  console.log({ result });
+});
 console.log('listening on port', process.env.PORT);
 agent.listen({ port: parseInt(process.env.PORT), handler });
