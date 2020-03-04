a2895ace45f8591dad1fe86fef90ddc0504bed05
Michiel de Jong <michiel@unhosted.org>
2020-03-04 16:49:58 +0100

    clean up example, fix #65, fix #66

--- test/unit/example.test.ts
+++ test/unit/example.test.ts
@@ -4,10 +4,8 @@ describe("Example", () => {
   it("runs", () => {
     const example = new Example();
     example.runExample();
-    expect(
-      example.redNode
-        .getChannelWatcher("alice", "bob", "10E-3 USD")
-        .getOurCurrent()
-    ).toEqual(-10);
+    expect(example.redNode.getBalances("alice", "bob", "10E-3 USD")).toEqual({
+      current: -10
+    });
   });
 });
